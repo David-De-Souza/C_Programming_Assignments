ea175544063b24fa7baa484cfa877c525c7f4a1c
David De Souza <70505948+David-De-Souza@users.noreply.github.com>
2024-01-26 21:44:03 +0800

    Assignment 6 specification

--- Assignment_06/products.c
+++ Assignment_06/products.c
@@ -5,8 +5,10 @@ void AddProductName(char products[][MAX_NAME_LENGTH], int *numProducts, const ch
 {
     if (*numProducts < MAX_PRODUCTS)
     {
-        strcpy(products[*numProducts], addName);
-        (*numProducts)++;
+        // Add the product to the list
+
+
+
         printf("%s added successfully.\n", addName);
     }
     else
@@ -26,48 +28,21 @@ void DisplayProductNames(char products[][MAX_NAME_LENGTH], int numProducts)
 
     // Copy product names to a temporary array
     char sortedNames[MAX_PRODUCTS][MAX_NAME_LENGTH];
-    for (int i = 0; i < numProducts; i++)
-    {
-        strcpy(sortedNames[i], products[i]);
-    }
 
-    // Bubble Sort
-    for (int i = 0; i < numProducts - 1; i++)
-    {
-        for (int j = 0; j < numProducts - i - 1; j++)
-        {
-            if (strcmp(sortedNames[j], sortedNames[j + 1]) > 0)
-            {
-                // Swap function
-                char temp[MAX_NAME_LENGTH];
-                strcpy(temp, sortedNames[j]);
-                strcpy(sortedNames[j], sortedNames[j + 1]);
-                strcpy(sortedNames[j + 1], temp);
-            }
-        }
-    }
+
+    // Sort the products (in lexographical order)
+
 
     // Display sorted product names
-    printf("\nDisplaying Product Names:\n");
-    for (int i = 0; i < numProducts; i++)
-    {
-        printf("%s\n", sortedNames[i]);
-    }
+
 }
 
 // Function to search for a product name
 void SearchProductName(char products[][MAX_NAME_LENGTH], int numProducts, const char *searchName)
 {
     int found = 0;
-    for (int i = 0; i < numProducts; i++)
-    {
-        if (strcmp(products[i], searchName) == 0)
-        {
-            printf("\nProduct found: %s\n", products[i]);
-            found = 1;
-            break;
-        }
-    }
+
+
 
     if (!found)
     {
@@ -79,21 +54,8 @@ void SearchProductName(char products[][MAX_NAME_LENGTH], int numProducts, const 
 void RemoveProductName(char products[][MAX_NAME_LENGTH], int *numProducts, const char *removeName)
 {
     int found = 0;
-    for (int i = 0; i < *numProducts; i++)
-    {
-        if (strcmp(products[i], removeName) == 0)
-        {
-            // Shift elements to fill the gap
-            for (int j = i; j < *numProducts - 1; j++)
-            {
-                strcpy(products[j], products[j + 1]);
-            }
-            (*numProducts)--;
-            printf("\n%s removed successfully.\n", removeName);
-            found = 1;
-            break;
-        }
-    }
+
+
 
     if (!found)
     {
